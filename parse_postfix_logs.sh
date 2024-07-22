6e42eb35c826cf7f4a992caee878cd3c6880f117
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2024-07-22 19:48:28 -0300

    add func bounced

--- parse_postfix_logs.sh
+++ parse_postfix_logs.sh
@@ -14,7 +14,7 @@ fi
 LOG_FILE="/var/log/mail.log"
 
 # Executar o comando Symfony como yuans5732
-su - yuans5732 -c "php $PROJECT_DIR/bin/console app:parse-postfix-logs $LOG_FILE"
+php $PROJECT_DIR/bin/console app:parse-postfix-logs $LOG_FILE
 
 # Limpar as filas de emails deferred como root
 postsuper -d ALL deferred
